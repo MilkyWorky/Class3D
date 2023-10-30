d0e95bb0d2276f453538433274b4ecf36e57c28d
MilkShook <phucbao98career@gmail.com>
2023-10-30 21:04:14 +0700

    first commit

---
+++ Assets/3D1-Collision/Script.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Script : MonoBehaviour
+{
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Hit! Trigger");
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        Debug.Log("Hit! Trigger");
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        Debug.Log("Hit! Trigger");
+    }
+
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("Hit! Collision");
+    }
+
+    private void OnCollisionStay(Collision collision)
+    {
+        Debug.Log("Hit! Collision");
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        Debug.Log("Hit! Collision");
+    }
+}
